890901afc1321457aa63ff433ed4e4fe6c8efe72
hamidreza-ramezani <hamid.ramezani1375@gmail.com>
2021-02-07 17:53:05 +0100

    sync all_reduce.h file with bits branch

--- src/collectives/device/all_reduce.h
+++ src/collectives/device/all_reduce.h
@@ -334,88 +334,120 @@ __device__ void ncclAllReduceRingKernel_new(struct CollectiveArgs* args) {
   }
 
   else {
-    const T * __restrict__ thisInput = (const T*)args->sendbuff;
-    T * __restrict__ thisOutput = (T*)args->recvbuff;
-    ncclPrimitives<UNROLL, ALLREDUCE_CHUNKSTEPS/ALLREDUCE_SLICESTEPS, ALLREDUCE_SLICESTEPS, T, 1, 1, 1, FUNC>                               
-      prims(tid, nthreads, &ring->prev, &ring->next, thisOutput, stepSize, channel, comm);                             
+     const T * __restrict__ thisInput = (const T*)args->sendbuff;
+     T * __restrict__ thisOutput = (T*)args->recvbuff;
 
-    for (ssize_t gridOffset = 0; gridOffset < size; gridOffset += nranks*loopSize) {
-      ssize_t realChunkSize = min(chunkSize, DIVUP(size-gridOffset,nranks*nChannels));
-      ALIGN_SIZE(realChunkSize, nthreads*sizeof(uint64_t)/sizeof(T));
-      ssize_t chunkOffset = gridOffset + bid*nranks*realChunkSize;
-      
+     ncclPrimitives<UNROLL, ALLREDUCE_CHUNKSTEPS/ALLREDUCE_SLICESTEPS, ALLREDUCE_SLICESTEPS, T, 1, 1, 1, FUNC>
+       prims(tid, nthreads, &ring->prev, &ring->next, thisOutput, stepSize, channel, comm);
 
-      /////////////// begin AllReduce steps ///////////////
-      ssize_t offset;
-      int nelem;
-      int chunk;
+     for (ssize_t gridOffset = 0; gridOffset < size; gridOffset += nranks*loopSize) {
+       //if (tid == 0 && blockIdx.x == 0 && ring->devUserRanks[0] == 0) {
+       //  printf("Hello world from old kernel\n");
+       //}
+       ssize_t realChunkSize = min(chunkSize, DIVUP(size-gridOffset,nranks*nChannels));
+       ALIGN_SIZE(realChunkSize, nthreads*sizeof(uint64_t)/sizeof(T));
+       ssize_t chunkOffset = gridOffset + bid*nranks*realChunkSize;
 
-      // step 0: push data to next GPU
-      chunk = ring->devUserRanks[nranks-1];
-      offset = chunkOffset + chunk * realChunkSize;
-      nelem = min(realChunkSize, size-offset);
+       //if (tid == 0 && blockIdx.x == 0 && ring->devUserRanks[0] == 0) {
+       //  printf("\nrealChunkSize %d\n", realChunkSize);
+       //}
 
 
-      const T* __restrict__ temp2 = (T*)comm->tempbuff3;               //when T != float
-      temp2 = compress<T>(thisInput, temp2, nelem);
-      prims.send(temp2+offset, nelem);
-      //prims.send(thisInput+offset, nelem);
+       /////////////// begin AllReduce steps ///////////////
+       ssize_t offset;
+       int nelem;
+       int chunk;
 
-      // k-2 steps: reduce and copy to next GPU
-      for (int j=2; j<nranks; ++j) {
-        chunk = ring->devUserRanks[nranks-j];
-        offset = chunkOffset + chunk * realChunkSize;
-        nelem = min(realChunkSize, size-offset);
+       // step 0: push data to next GPU
+       chunk = ring->devUserRanks[nranks-1];
+       offset = chunkOffset + chunk * realChunkSize;
+       nelem = min(realChunkSize, size-offset);
 
-        T* __restrict__ temp = (T*)comm->tempbuff1;			//when T != float
+       //if(tid == 0 && blockIdx.x == 0) {
+       //   printf("in the place 1\n");
+       //   printf("in device:%d\n", ring->devUserRanks[0]);
+       //   printf("offset is %d\n", offset);
+       //   printf("nelem is %d\n", nelem);
+       //   printf("\n\n\n");
+       //}
+       //__syncthreads();
 
-        prims.recv(temp + offset , nelem);
-        for (int idx = offset+tid; idx < offset+nelem; idx += args->coll.nThreads) {
-          //temp[idx] = FUNC()(temp[idx], thisInput[idx]);
-          //temp[idx] = FUNC()((T)temp[idx], thisInput[idx]);
-          T var = FUNC()((T)temp[idx], thisInput[idx]);
-          temp[idx] = compress<T>(var);
-        }
-        prims.send(temp + offset, nelem);
-        //prims.recvReduceSend(thisInput+offset, nelem);
-      }
+       prims.send(thisInput+offset, nelem);
+       for (int j=2; j<nranks; ++j) {
+         chunk = ring->devUserRanks[nranks-j];
+         offset = chunkOffset + chunk * realChunkSize;
+         nelem = min(realChunkSize, size-offset);
+         
+         //if(tid == 0 && blockIdx.x == 0) {
+         //   printf("in the place 2 and j %d\n", j);
+         //   printf("in device:%d\n", ring->devUserRanks[0]);
+         //   printf("offset is %d\n", offset);
+         //   printf("nelem is %d\n", nelem);
+         //   printf("\n\n\n");
+         //}
+         //__syncthreads();
 
-      chunk = ring->devUserRanks[0];
-      offset = chunkOffset + chunk * realChunkSize;
-      nelem = min(realChunkSize, size-offset);
+         //prims.recv(thisOutput+offset, nelem);
+         //prims.send(thisInput+offset, nelem);
+         prims.recvReduceSend(thisInput+offset, nelem);
+       }
+
+       // step k-1: reduce this buffer and data, which will produce the final
+       // result that we store in this data and push to the next GPU
+       chunk = ring->devUserRanks[0];
+       offset = chunkOffset + chunk * realChunkSize;
+       nelem = min(realChunkSize, size-offset);
+       
+       //if(tid == 0 && blockIdx.x == 0) {
+       //   printf("in the place 3\n");
+       //   printf("in device:%d\n", ring->devUserRanks[0]);
+       //   printf("offset is %d\n", offset);
+       //   printf("nelem is %d\n", nelem);
+       //   printf("\n\n\n");
+       //}
+       //__syncthreads();
+
+       //prims.directRecv(thisOutput+offset, offset, nelem);
+       //prims.copySend(thisInput+offset, thisOutput+offset, nelem);
+
+       prims.directRecvReduceCopySend(thisInput+offset, thisOutput+offset, offset, nelem);
+
+       // k-2 steps: copy to next GPU
+       for (int j=1; j<nranks-1; ++j) {
+         chunk = ring->devUserRanks[nranks-j];
+         offset = chunkOffset + chunk * realChunkSize;
+         nelem = min(realChunkSize, size-offset);
 
 
-     //T* __restrict__ temp = (T*)args->tempbuff1;                  //when T != float
-      T* __restrict__ temp = (T*)comm->tempbuff1;                  //when T != float
+         //if(tid == 0 && blockIdx.x == 0) {
+         //   printf("in the place 4 and j %d\n", j);
+         //   printf("in device:%d\n", ring->devUserRanks[0]);
+         //   printf("offset is %d\n", offset);
+         //   printf("nelem is %d\n", nelem);
+         //   printf("\n\n\n");
+         //}
+         //__syncthreads();
 
-      prims.directRecv(temp + offset , offset, nelem);
-      for (int idx = offset+tid; idx < offset+nelem; idx += args->coll.nThreads) {
-        //temp2[idx] = FUNC()(thisInput[idx], temp2[idx]);
-        //temp[idx] = FUNC()(thisInput[idx], (T)temp[idx]);
-        T var = FUNC()((T)temp[idx], thisInput[idx]);
-        temp[idx] = compress<T>(var);
-      }
+         prims.directRecvCopySend(thisOutput+offset, offset, nelem);
+       }
 
-      prims.copySend(temp + offset, thisOutput+offset, nelem);
+       // Make final copy from buffer to dest.
+       chunk = ring->devUserRanks[1];
+       offset = chunkOffset + chunk * realChunkSize;
+       nelem = min(realChunkSize, size-offset);
 
-      //prims.directRecvReduceCopySend(thisInput+offset, thisOutput+offset, offset, nelem);
 
-      // k-2 steps: copy to next GPU
-      for (int j=1; j<nranks-1; ++j) {
-        chunk = ring->devUserRanks[nranks-j];
-        offset = chunkOffset + chunk * realChunkSize;
-        nelem = min(realChunkSize, size-offset);
+       //if(tid == 0 && blockIdx.x == 0) {
+       //   printf("in the place 3\n");
+       //   printf("in device:%d\n", ring->devUserRanks[0]);
+       //   printf("offset is %d\n", offset);
+       //   printf("nelem is %d\n", nelem);
+       //   printf("\n\n\n");
+       //}
+       //__syncthreads();
 
-        prims.directRecvCopySend(thisOutput+offset, offset, nelem);
-      }
-
-      // Make final copy from buffer to dest.
-      chunk = ring->devUserRanks[1];
-      offset = chunkOffset + chunk * realChunkSize;
-      nelem = min(realChunkSize, size-offset);
-
-      // Final wait/copy.
-      prims.directRecv(thisOutput+offset, offset, nelem);
+       // Final wait/copy.
+       prims.directRecv(thisOutput+offset, offset, nelem);
     }
   }
 }
