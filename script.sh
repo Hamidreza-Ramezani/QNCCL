8e244ef7f04bddbc3c6336071496efa9c7ae8cb5
hamid-ramezani <hamidreza.ramezanikebrya@ist.ac.at>
2021-08-23 19:56:18 +0200

    figure 4 is added

--- script.sh
+++ script.sh
@@ -1,5 +1,6 @@
 #!/usr/bash
 
+
 make clean
 #export DEBUG=1
 #export NCCL_DEBUG=INFO
@@ -7,10 +8,6 @@ make clean
 #export NCCL_DEBUG_SUBSYS
 
 #export CUDA_HOME=/mnt/nfs/clustersw/shared/cuda/10.1.243
-export CUDA_HOME=/mnt/nfs/clustersw/shared/cuda/11.0.3
+export CUDA_HOME=/mnt/nfs/clustersw/shared/cuda/11.2.0
 
 make -j src.build NVCC_GENCODE="-gencode=arch=compute_80,code=sm_80"
-
-
-
-
